26e41226503d550d9a69e50cbb61e71d40e20e83
saiusesgithub <saisrujantesting@gmail.com>
2025-09-19 23:15:40 +0530

    feat: update configuration for next export and add deployment scripts

--- next.config.mjs
+++ next.config.mjs
@@ -11,4 +11,14 @@ const nextConfig = {
   },
 }
 
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',            // enables `next export`
+  images: { unoptimized: true }, // required for export when using next/image
+  trailingSlash: true
+};
+module.exports = nextConfig;
+
+
 export default nextConfig

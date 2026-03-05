75b5217efa47c7b58ef5bd7cc7bd81b296f582e2
saiusesgithub <saisrujanpunati@gmail.com>
2026-03-05 19:37:55 +0530

    feat: add Terraform configuration for AWS infrastructure including EC2 instance, security group, and outputs

---
+++ terraform/compute.tf
@@ -0,0 +1,25 @@
+resource "aws_instance" "nginx_server" {
+  ami           = "ami-0fb67e6212e8ff822"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Portfolio"
+  }
+  key_name = "portfolio-terraform-key"
+  vpc_security_group_ids = [aws_security_group.portfolio_security_group.id]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt update
+    sudo apt install docker.io -y
+    sudo systemctl start docker
+    sudo systemctl enable docker
+    sudo docker pull saiusesdocker/portfolio
+    sudo docker run -d -p 80:80 saiusesdocker/portfolio
+  EOF
+}
+
+resource "aws_eip" "public_ip" {
+  instance = aws_instance.nginx_server.id
+  domain   = "vpc"
+}

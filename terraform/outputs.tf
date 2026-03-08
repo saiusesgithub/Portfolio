0272d772a8975109068c8d4815bfa5e7b6341a52
saiusesgithub <saisrujanpunati@gmail.com>
2026-03-08 16:18:47 +0530

    feat: integrate Ansible for deployment, update inventory and playbook configurations

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,6 +8,6 @@ resource "local_file" "ansible_inventory" {
 
   content = <<EOF
 [web]
-server ansible_host=${aws_eip.public_ip.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=portfolio-terraform-key.pem
+server ansible_host=${aws_eip.public_ip.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/ec2_key.pem
 EOF
 }
